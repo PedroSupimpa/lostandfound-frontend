65ac5a59426c1732cbeca893531d238edb89e55c
Pedro Souza <shiro2303@gmail.com>
2024-04-05 00:10:48 -0300

    Add navigation to signup page

--- lostandfound/src/pages/home/index.tsx
+++ lostandfound/src/pages/home/index.tsx
@@ -1,11 +1,23 @@
+import {  useNavigate } from "react-router-dom";
+
 
 const Home = () => {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/signup'); 
+  };
   return (
-    <div className="w-5 h-5">
-      <h1 className="text-red-500">Home</h1>
-      <h2 className="text-blue-600">TESTEEEEEEEEEEEEEEEEEEEEEEEE</h2>
-    </div>
+    <div className="flex justify-end px-3 pt-2">
+
+  <button onClick={handleClick}
+    className="bg-emerald-500 rounded font-semibold text-white h-10 w-fit p-2 hover:bg-emerald-600">
+      Go to Signup
+    </button>
+      </div>
   );
+  
 };
 
 export default Home;

4d0543b2ad507edca23f59ede80076a25e8a4697
Pedro Souza <shiro2303@gmail.com>
2024-05-12 18:57:13 -0300

    Refactor ItemCard component and improve loading state handling

--- lostandfound/src/components/ItemCard/index.tsx
+++ lostandfound/src/components/ItemCard/index.tsx
@@ -16,55 +16,58 @@ interface ItemCardProps {
 const ItemCard = ({ postData }: ItemCardProps) => {
   const [isLoading, setIsLoading] = useState(true);
 
+  const isMobile = window.innerWidth < 768;
+
   useEffect(() => {
     if (postData) {
       setIsLoading(false);
     }
   }, [postData]);
 
-  return (
-    isLoading ? (
-      <div>
+  
+
+  
+
+  return isLoading ? (
+    <div>
       <Card className="m-6 flex md:container md:mx-auto">
-          <div className="flex  space-x-4 p-4 w-full">
-            <Skeleton className="h-[120px] w-[130px] " />
-            <div className="space-y-2 w-full">
-              <Skeleton className="h-10 w-[40%]" />
+        <div className="flex  space-x-4 p-4 w-full">
+          <Skeleton className="h-[120px] w-[130px] " />
+          <div className="space-y-2 w-full">
+            <Skeleton className="h-10 w-[40%]" />
 
-
-              <Skeleton className="h-4 w-[50%]" />
-              <Skeleton className="h-4 w-[50%]" />
-
-            </div>
+            <Skeleton className="h-4 w-[50%]" />
+            <Skeleton className="h-4 w-[50%]" />
           </div>
-        </Card>
-      </div>
-    ) : (
-      <div key={postData.id}>
-        <        Card className="m-6 flex md:container md:mx-auto">
-          <CardContent className="relative w-full md:w-[15vw] md:pb-[10vw] overflow-hidden">
-
-            <img
-              src={false ? "" : "/images/placeholder.png"}
-              alt=""
-              className="absolute top-0 left-0 h-full w-full object-cover"
-            />
-          </CardContent>
-          <div className="m-2">
-            <CardHeader className="h-1/3">
-                <CardTitle>{postData.title}</CardTitle>
-              </CardHeader>
-              <CardDescription className="overflow-hidden h-1/3">
-                {postData.description}
-              </CardDescription>
-              <div className="px-5 flex items-baseline justify-end gap-4 h-1/3">
-                <p>{"category"}</p>
-                <p>{"location"}</p>
-              </div>
-            </div>
-          </Card>
         </div>
-      )
+      </Card>
+    </div>
+  ) : (
+    <div key={postData.id}>
+      <Card className={`my-5 flex md:container md:mx-auto ${isMobile ? 'h-[28vw]':'h-[10vw]'}`}>
+        <CardContent className="relative min-w-[120px] md:w-[15vw] md:pb-[10vw] overflow-hidden">
+          <img
+            src={ ` ${postData ? postData.images.map((image) => image.imageLink)[0] : '/images/placeholder.png'}`}
+            alt=""
+            className="absolute top-0 left-0 h-full w-full object-cover"
+          />
+        </CardContent>
+        <div className="m-2 w-full flex flex-col">
+          <CardHeader className="flex flex-grow p-0">
+            <CardTitle>{postData.title}</CardTitle>
+          </CardHeader>
+          {!isMobile && (
+            <CardDescription className="flex-grow overflow-hidden">
+              {postData.description}
+            </CardDescription>
+          )}
+          <div className="flex flex-grow-0 h-[20%] items-end justify-between p-0 overflow-hidden">
+            <p>{"Taguatinga - DF"}</p>
+            <p>{postData.category.name}</p>
+          </div>
+        </div>
+      </Card>
+    </div>
   );
 };
 
